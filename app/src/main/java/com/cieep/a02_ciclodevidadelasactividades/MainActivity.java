03b030674d83ca70939b4d6dfc32c7e70ead3ccd
eduard <eduard@cieep.com>
2022-09-20 11:57:36 +0200

    Funciones creadas

--- app/src/main/java/com/cieep/a02_ciclodevidadelasactividades/MainActivity.java
+++ app/src/main/java/com/cieep/a02_ciclodevidadelasactividades/MainActivity.java
@@ -3,6 +3,7 @@ package com.cieep.a02_ciclodevidadelasactividades;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.util.Log;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -10,5 +11,41 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+    }
+
+    @Override
+    protected void onStart() {
+        super.onStart();
+        Log.e("CICLO VIDA", "Estoy ejecutando onStart");
+    }
+
+    @Override
+    protected void onRestart() {
+        super.onRestart();
+        Log.e("CICLO VIDA", "Estoy ejecutando onRestart");
+    }
+
+    @Override
+    protected void onStop() {
+        super.onStop();
+        Log.e("CICLO VIDA", "Estoy ejecutando onStop");
+    }
+
+    @Override
+    protected void onDestroy() {
+        super.onDestroy();
+        Log.e("CICLO VIDA", "Estoy ejecutando onDestroy");
+    }
+
+    @Override
+    protected void onPause() {
+        super.onPause();
+        Log.e("CICLO VIDA", "Estoy ejecutando onPause");
+    }
+
+    @Override
+    protected void onResume() {
+        super.onResume();
+        Log.e("CICLO VIDA", "Estoy ejecutando onResume");
     }
 }
